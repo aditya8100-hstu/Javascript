e2eeedd345a5709325197a320349f051a5ae9b9a
aditya8100-hstu <58524511+aditya8100-hstu@users.noreply.github.com>
2021-06-18 10:59:54 +0600

    Add files via upload

---
+++ Javascript Basic/practice.js
@@ -0,0 +1,45 @@
+/* This is a Javascript Small Project by which you can learn Javascript Basic
+Operition likes  variable, Operator , Devide two numbers, Multiply two numbers , increment operator,
+decrement operator and so on. */
+
+var num1 = 10; // Declare Javascript Variables by "var" keyword and assign value to the variable
+var num2;
+num2 = num1; // Assigning the value of one variable to another
+
+document.write("The value of number1 is : " +num2 + "<br> <br>");
+var num3 = 20
+var addition = num1+num3;  //Addition of two decimal number
+var subtraction = num1-num3; //Subtraction of two decimal number
+var multiplication = num1 * num3; //Multiplication of two decimal number
+var dividation = num1 /num2; //Dividation of two decimal number
+document.write(" Addition of two decimal number : " +addition +"<br>"); 
+document.write("Subtraction of two decimal number : " +subtraction +"<br>");
+document.write("Multiplication of two decimal number : " +multiplication +"<br>");
+document.write("Dividation of two decimal number: " +dividation +"<br><br>");
+
+
+//document.write("<br>");
+
+/* We can get the remainder of two number by Javascript using % operator */
+var remainder = 11%3;
+document.write("The remainder of two number is : " + remainder +"<br><br>");
+
+/* We can also assign a deceimal number into a variable by Javascript */
+
+var myDecimal = 5.7;
+document.write("The decimal value is : " +myDecimal +"<br><br>");
+
+var myDecimal2 = 1.3;
+
+var addition = myDecimal+myDecimal2; //Addition of two floating number
+var subtraction = myDecimal-myDecimal2; //Subtraction of two floating number
+var multiplication = myDecimal * myDecimal2; //Multiplication of two floating number
+var dividation = myDecimal /myDecimal2; //Dividation of two floating number
+document.write("Addition of two floating number : " +addition +"<br>");  
+document.write("Subtraction of two floating number: " +subtraction +"<br>");
+document.write("Multiplication of two floating number : " +multiplication +"<br>");
+document.write("Dividation of two floating number: " +dividation +"<br>");
+
+
+
+
