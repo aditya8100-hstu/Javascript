87b3f0d1b3ea13962cc06b204df17ade683fb552
aditya8100-hstu <58524511+aditya8100-hstu@users.noreply.github.com>
2021-06-27 12:22:18 +0600

    Javascript basic object
    basic object properties

---
+++ Javascript Objects/thisObject.js
@@ -0,0 +1,23 @@
+/*If the variable name changes, any code referencing the original name would need to be updated as well. 
+In a short object definition, it isn't a problem, but if an object has many references to its properties there is a greater chance for error.
+That means if the object name is changed then we don't have to change all the object name 
+which we use in our code if we use "this" keyword. */
+
+let student = {
+    firstName : "Aditya",
+    lastName : "Chakrabortty",
+    age : 23,
+    address : "Rangpur",
+
+    sentence: function()
+        {return "I am " +this.firstName+ " " +this.lastName+ " and I'm " +this.age+ "years old"
+    }  
+        
+   /* sentence:function(){
+        return "I am " +student.firstName+ " " +student.lastName+ " and I'm " +student.age+ "years old"
+    } */
+
+};
+
+newline = student.sentence();
+document.write(newline);
