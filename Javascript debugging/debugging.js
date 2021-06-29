fcc84ee7854d7eeb8aa813acdeb88672da8d1fc9
aditya8100-hstu <58524511+aditya8100-hstu@users.noreply.github.com>
2021-06-29 12:19:02 +0600

    Add files via upload

---
+++ Javascript debugging/debugging.js
@@ -0,0 +1,20 @@
+//Debugging in javascript
+
+//Use typeof to Check the Type of a Variable
+let a = "34";
+let b = 34;
+document.write("The type is: " +typeof(a)+ "<br>");
+document.write("The type is: " +typeof(b)+ "<br>");
+
+
+// Mixed Usage of Single and Double Quotes
+const grouchoContraction = "I've had a perfectly wonderful evening, but this wasn't it.";
+const quoteInString = "Groucho Marx once said 'Quote me as saying I was mis-quoted.'";
+const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it.';
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+document.write(grouchoContraction +"<br>");
+document.write(quoteInString +"<br>");
+document.write(allSameQuotes +"<br>");
+document.write(innerHtml +"<br>");
+
+
