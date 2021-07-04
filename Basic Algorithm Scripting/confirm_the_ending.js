decf2d35afaba1664df688fd40b0b1a4cdbff566
aditya8100-hstu <58524511+aditya8100-hstu@users.noreply.github.com>
2021-07-04 11:38:51 +0600

    Confirm the Ending

---
+++ Basic Algorithm Scripting/confirm_the_ending.js
@@ -0,0 +1,11 @@
+
+document.write("<br>");
+
+  function confirmEnding(str, target) {
+   
+    return str.endsWith(target);
+  }
+  
+  let b = confirmEnding("Bastian", "n");
+
+  document.write(b);
